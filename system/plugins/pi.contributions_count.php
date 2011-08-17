c03b0381b78654e2a4a7a856d1d33c491e4a8782
avalla <valla.andrea@gmail.com>
2011-08-17 19:59:10 +0300

    updated for EE2

--- system/plugins/pi.contributions_count.php
+++ system/plugins/pi.contributions_count.php
@@ -1,35 +1,34 @@
 <?php
 
 $plugin_info = array(
-	'pi_name'			=> 'Contributions Count',
+	'pi_name'			=> 'Contributions Count EE2',
 	'pi_version'		=> '1.0',
 	'pi_author'			=> 'Nine Four',
 	'pi_author_url'		=> 'http://ninefour.co.uk/labs',
 	'pi_description'	=> 'Returns the number of contributions made by a member to a specific weblog, something not possible with native tags.',
-	'pi_usage'			=> ''
+	'pi_usage'			=> contributions_count::usage()
 );
 
 class contributions_count {
-
+	var $return_data;
 	function contributions_count() {
-		global $DB, $TMPL;
+                $this->EE =& get_instance();
 		
 		//collect our parameters
-		$member_id = $TMPL->fetch_param('member_id');
-		$weblog_id = $TMPL->fetch_param('weblog_id');
-		$status = $TMPL->fetch_param('status');
-		$link_url = $TMPL->fetch_param('link_url');
-		$link_text = $TMPL->fetch_param('link_text');
-		$link_title = $TMPL->fetch_param('link_title');
-		$link_class = $TMPL->fetch_param('link_class');
-		
-		$sql = "SELECT entry_id FROM exp_weblog_titles
-				WHERE author_id=".$member_id." AND weblog_id=".$weblog_id." AND status='".$status."'";
-		$query = $DB->query($sql);
+		$author_id = $this->EE->TMPL->fetch_param('author_id');
+		$channel_id = $this->EE->TMPL->fetch_param('channel_id');
+		$status = $this->EE->TMPL->fetch_param('status');
+		$link_url = $this->EE->TMPL->fetch_param('link_url');
+		$link_text = $this->EE->TMPL->fetch_param('link_text');
+		$link_title = $this->EE->TMPL->fetch_param('link_title');
+		$link_class = $this->EE->TMPL->fetch_param('link_class');
+
+		$sql = "SELECT T.entry_id FROM exp_channel_data AS D , exp_channel_titles AS T WHERE T.site_id = '1' AND T.entry_id = D.entry_id AND T.author_id = '" . $author_id . "' AND T.status = '" . $status . "' AND D.channel_id = '" . $channel_id . "'";
+                $query = $this->EE->db->query($sql);
 		if ($query->num_rows > 0) {
-			
+
 			$data = $query->num_rows;
-			
+
 			if ($link_url!="" AND $link_text!="") {
 				$data .= "<a href='".$link_url."'";
 				if ($link_title!="") {
@@ -43,10 +42,14 @@ class contributions_count {
 		} else {
 			$data = '0';
 		}
-		
+
 		$this->return_data = $data;
 		return $this->return_data;
 	}
-	
+	function usage() {
+                $data = "{exp:contributions_count author_id=\"\" channel_id=\"\" status=\"\" link_url=\"\" link_text=\"\" link_title=\"\" link_class=\"\"}";
+                $this->return_data = $data;
+                return $this->return_data;
+	}
 }
 ?>
